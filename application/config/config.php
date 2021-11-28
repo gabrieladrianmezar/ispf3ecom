6797c3f39a694ab23272015ded2e50857d21fa0b
Gabriel AMR <85672399+gabrieladrianmezar@users.noreply.github.com>
2021-11-28 20:38:50 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/isfp3ecom/';
+$config['base_url'] = 'https://isfp3ecom.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
